9bdac3adb1bc845bafc467d506cddb0a003f8b1f
Kristen Goehringer <kgoehringer@gmail.com>
2017-07-31 12:11:46 +0200

    Added DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -41,11 +41,29 @@ app.get('/todos/:id', (req, res) => {
 			console.log('Could not find todo');
 			return res.status(404).send();
 		}
-		res.send({todo})
+		res.send({todo});
 	}).catch((e) => {
 		res.status(404).send();
 	});
 });
+
+app.delete('/todos/:id', (req, res) => {
+	var id = req.params.id;
+	if(!ObjectID.isValid(id)){
+		return res.status(404).send();
+	}
+
+	Todo.findByIdAndRemove(id).then((todo) => {
+		if(!todo){
+			return res.status(400).send('Couldnt find todo. Not deleted.');
+		}
+		
+		res.send({todo});
+	}).catch((e) => {
+		res.status(400).send();
+	});
+});
+
 
 
 app.listen(port, () => {
